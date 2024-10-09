238cf13bba047804daf095a6e99eb8357a509d86
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-09 09:33:27 +0100

    Update notification.jsx

--- components/navbar/notification/notification.jsx
+++ components/navbar/notification/notification.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import Image from "next/image";
+
+import Style from "./notification.module.css";
+import images from "../../../public/img";
 
 export default function Notificationc() {
-  return <div>notification</div>;
+  return (
+    <div className={Style.notification}>
+      <p>Notification</p>
+      <div className={Style.notification_box}>
+        <div className={Style.notification_box_img}>
+          <Image
+            src={images.user1}
+            alt="profile image"
+            width={50}
+            height={50}
+            className={Style.notification_box_img}
+          />
+        </div>
+        <div className={Style.notification_box_info}>
+          <h4>Shoaib Akhter</h4>
+          <p>Measure action your user...</p>
+          <small>3 minutes ago</small>
+        </div>
+        <span className={Style.notification_box_new}></span>
+      </div>
+    </div>
+  );
 }
