8d8c8c19f8e856865a1dcf0ccc2353b906516de3
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-09 09:20:37 +0100

    Update help-center.jsx

--- components/navbar/help-center/help-center.jsx
+++ components/navbar/help-center/help-center.jsx
@@ -1,5 +1,38 @@
 import React from "react";
+import Link from "next/link";
+
+import Style from "./help-center.module.css";
 
 export default function HelpCenter() {
-  return <div>help-center</div>;
+  const helpCenter = [
+    {
+      name: "About",
+      link: "aboutus",
+    },
+    {
+      name: "Contact Us",
+      link: "contactus",
+    },
+    {
+      name: "Sign Up",
+      link: "signUp",
+    },
+    {
+      name: "LogIn",
+      link: "login",
+    },
+    {
+      name: "Subscription",
+      link: "subscription",
+    },
+  ];
+  return (
+    <div className={Style.box}>
+      {helpCenter.map((el, i) => (
+        <div className={Style.helpCenter} key={i + 1}>
+          <Link href={{ pathname: `${el.link}` }}>{el.name}</Link>
+        </div>
+      ))}
+    </div>
+  );
 }
