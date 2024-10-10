4378c0a78c91d14f6bab9fcc464c7278ea53a159
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-10-10 09:21:03 +0100

    Create index.js

---
+++ constants/index.js
@@ -0,0 +1,57 @@
+export const discover = [
+  {
+    name: "Collection",
+    link: "collection",
+  },
+  {
+    name: "Search",
+    link: "searchPage",
+  },
+  {
+    name: "Author Profile",
+    link: "author",
+  },
+  {
+    name: "NFT Details",
+    link: "NFT-details",
+  },
+  {
+    name: "Account Setting",
+    link: "account",
+  },
+  {
+    name: "Upload NFT",
+    link: "uploadNFT",
+  },
+  {
+    name: "Connect Wallet",
+    link: "connectWallet",
+  },
+  {
+    name: "Blog",
+    link: "blog",
+  },
+];
+
+export const helpCenter = [
+  {
+    name: "About",
+    link: "aboutus",
+  },
+  {
+    name: "Contact Us",
+    link: "contactus",
+  },
+  {
+    name: "Sign Up",
+    link: "signUp",
+  },
+  {
+    name: "LogIn",
+    link: "login",
+  },
+  {
+    name: "Subscription",
+    link: "subscription",
+  },
+];
